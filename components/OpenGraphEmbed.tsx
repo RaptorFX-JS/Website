5c64b277a58ff3bf6610a83d262c1bbc0c87f8b5
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2023-04-24 16:18:33 +0530

    feat: refactor meta and test roadmap page

--- components/OpenGraphEmbed.tsx
+++ components/OpenGraphEmbed.tsx
@@ -3,42 +3,58 @@ import { Head } from "aleph/react"
 export interface IEmbed {
 	title?: string
 	description?: string
+	url?: string | undefined
 	image?: string
 }
 
-export default function Embed(embed: IEmbed) {
+export default function Embed(props: IEmbed) {
+	const ROOT = "https://raptorfx.deno.dev"
+	props.title = props.title ??
+		"RaptorFX | A Modern-Denofied Solution to Building Apps!"
+	props.description = props.description ??
+		"Welcome to RaptorFX, the Modern App development tool using Web Technologies!"
+	props.url = props.url ?? ROOT
+	props.image = props.image ?? `${ROOT}/assets/logo.png`
+
 	return (
 		<Head>
-			<title>
-				{embed.title ??
-					"RaptorFX | A Modern-Denofied Solution to Building Apps!"}
-			</title>
+			<title>{props.title}</title>
 			<meta
 				name="description"
-				content={
-					embed.description ??
-					"Welcome to RaptorFX, the modern app development tool."
-				}
+				content={props.description}
 			/>
+
+			<meta property="og:url" content={props.url} />
+			<meta property="og:type" content="website" />
 			<meta
 				property="og:title"
-				content={
-					embed.title ??
-					"RaptorFX | A Modern-Denofied Solution to Building Apps!"
-				}
+				content={props.title}
 			/>
 			<meta
 				property="og:description"
-				content={embed.description ?? "Welcome to RaptorFX Website!"}
+				content={props.description}
 			/>
 			<meta
 				property="og:image"
-				content={
-					embed.image ?? "https://raptorfx.deno.dev/assets/logo.png"
-				}
+				content={props.image}
 			/>
-			<meta property="og:url" content="https://raptorfx.deno.dev/" />
-			<meta name="theme-color" content="#FFFFFF" />		
+			<meta name="theme-color" content="#FFFFFF" />
+
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta property="twitter:domain" content="quickstayrooms.com" />
+			<meta property="twitter:url" content={props.url} />
+			<meta
+				name="twitter:title"
+				content={props.title}
+			/>
+			<meta
+				name="twitter:description"
+				content={props.description}
+			/>
+			<meta
+				name="twitter:image"
+				content={props.image}
+			/>
 		</Head>
 	)
 }
--- routes/blog.tsx
+++ routes/blog.tsx
@@ -3,9 +3,23 @@ import { MDXProvider } from "@mdx-js/react"
 import { components } from "~/components/Heading.tsx"
 
 export default function Blog(props: PropsWithChildren) {
-    return <div className="min-h-[80vh] p-2">
-        <MDXProvider components={components}>
-            {props.children}
-        </MDXProvider>
-    </div>
+	return (
+		<div className="min-h-[80vh] p-2 md:px-32">
+            <style>
+                {
+                    `h1 {
+                        font-size: 1.5rem;
+                    }
+                    p:has(img) {
+                        display: flex;
+                        justify-content: center;
+                    }
+                    `
+                }
+            </style>
+			<MDXProvider components={components}>
+				{props.children}
+			</MDXProvider>
+		</div>
+	)
 }
